85b8dccad8c889af97c2782e2ee60d5428026cf5
Kedwin Chen <kedwinchen@gmail.com>
2018-11-15 11:44:01 -0800

    20181115 kchen75-orchestrator init
    Start working on a basic frame for an orchestrator.

--- src/main/control/Orchestrator.js
+++ src/main/control/Orchestrator.js
@@ -1,3 +1,30 @@
 export default class Orchestrator {
+  constructor() {
+    this.scheduledMissions = [];
+    this.currentMission = null;
+    this.nextMission = null;
+    this.passToNext = null;
 
+    this.knownVehicles = [];
+  }
+
+  getScheduledMissions() {
+    return this.scheduledMissions;
+  }
+
+  getKnownVehicles() {
+    return this.knownVehicles;
+  }
+
+  addVehicle(vehicle) {
+    this.knownVehicles.push(vehicle);
+  }
+
+  addMission(mission) {
+    if (mission instanceof Mission) {
+      this.scheduledMissions.push(mission);
+    } else {
+      console.warn('In Class `Orchestrator`, method `addMission`: Received an object constructed with', mission.constructor.name);
+    }
+  }
 }
