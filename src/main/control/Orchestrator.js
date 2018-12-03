f1a2d3817fda6cb60911621143b9c373bc9b9c77
Kedwin Chen <kedwinchen@gmail.com>
2018-12-02 18:44:38 -0800

    20181202 kchen75_orchestrator Add receive message logic
    Add some basic logic for handling received messages. This obviously
    requires more work.
    
    Also need to add more logic for handling the sending of messages
    received from each Mission.
    
    Also need to stand up a basic frame for the Vehicle type.

--- src/main/control/Orchestrator.js
+++ src/main/control/Orchestrator.js
@@ -1,43 +1,154 @@
+import { ipcRenderer } from 'electron';
+
 export default class Orchestrator {
+
+  static reportFailure(failureMessage) {
+    const ipcMessage = {
+      type: 'failure',
+      message: failureMessage,
+    };
+    console.log('FAILURE: ', failureMessage);
+    ipcRenderer.send('post', 'updateMessages', ipcMessage);
+  }
+
+  /**
+    *   Creates an instance of an Orchestrator
+    *   @constructor
+    *   @this {Orchestrator}
+    */
   constructor() {
     this.scheduledMissions = [];
     this.currentMission = 0;
-    this.nextMission = null;
-    this.passToNext = null;
-
+    this.nextMissionRequiredData = null;
     this.knownVehicles = [];
   }
 
+  /**
+    *   Getter for the scheduledMissions member variable.
+    *   @this {Orchestrator}
+    *   @returns {list} scheduledMissions: The list of scheduled Missions
+    */
   getScheduledMissions() {
     return this.scheduledMissions;
   }
 
+  /**
+    *   Getter for the knownVehicles member variable.
+    *   @this {Orchestrator}
+    *   @returns {list} knownVehicles: The list of known Vehicles.
+    */
   getKnownVehicles() {
     return this.knownVehicles;
   }
 
+  /**
+    *   Adds a vehicle to the known vehicle list.
+    *   @this {Orchestrator}
+    *   @param {Vehicle} vehicle:   The vehicle to add to the list
+    */
   addVehicle(vehicle) {
     this.knownVehicles.push(vehicle);
   }
 
+  /**
+    *   Marks a vehicle not active and unavailable for tasking.
+    *   @this {Orchestrator}
+    *   @param {Vehicle} vehicle:   The vehicle to deactivate
+    */
+  deactivateVehicle(vehicle) {
+    vehicle.setActive(false);
+    vehicle.setAvailable(false);
+  }
+
+  /**
+    *   Adds a mission to be executed.
+    *   @TODO: Do ordering on the missions (e.g., quickSearch should be executed before detailedSearch)
+    *   @this {Orchestrator}
+    *   @param {Mission} mission: The mission to be added.
+    */
   addMission(mission) {
     if (mission instanceof Mission) {
       this.scheduledMissions.push(mission);
     } else {
-      console.warn('In Class `Orchestrator`, method `addMission`: Received an object constructed with', mission.constructor.name);
+      Orchestrator.log('In Class `Orchestrator`, method `addMission`: Received an object constructed with: ', mission.constructor.name, ' ; expected object of type `Mission` or subclass');
     }
   }
-  
-  startMission(mission, requiredData) {
-	  this.scheduledMissions[this.currentMission].start(requiredData);
-	}
 
+  /**
+    *   Checks a Mission, then starts it.
+    *   @this {Orchestrator}
+    *   @param {JSON} requiredData: The data required by the current Mission (the Mission that is to be started).
+    */
+  startMission(requiredData) {
+    const missionOK = this.scheduledMissions[this.currentMission].check(this.knownVehicles, requiredData);
+    if (missionOK) {
+      this.scheduledMissions[this.currentMission].start();
+    } else {
+      Orchestrator.log(this.currentMissionName(), ' mission could not be started due to a failure in the internal checks!');
+    }
+  }
+
+  /**
+    *   Ends a Mission
+    *   @this {Orchestrator}
+    *   @param {JSON} nextRequired: The data required by the next Mission (the next Mission that is to be started).
+    */
   endMission(nextRequired) {
-    this.passToNext = nextRequired;
+    this.nextMissionRequiredData = nextRequired;
     this.currentMission++;
   }
-  
+
+  /**
+    *   Getter for the name of the current Mission
+    *   @this {Orchestrator}
+    *   @returns {string} currentMissionName: the name of the current Mission
+    */
   currentMissionName() {
     return this.scheduledMissions[this.currentMission].getName();
+  }
+
+  /**
+    *   Sends/Schedules to send a message to a Vehicle.
+    *   @this {Orchestrator}
+    *   @param {string} vehicleID: the unique ID (UID) for the vehicle to send the `message` to.
+    *   @param {JSON} message: the message to send to the vehicle with UID `vehicleID`
+    */
+  sendMessage(vehicleID, message) {
+    this.selectVehicle(vehicleID).sendMessage(message);
+  }
+
+  /**
+    *   Processes a given message from a vehicle
+    *   @TODO: Add support for messages from other vehicles (only VTOL currently)
+    *   @this {Orchestrator}
+    *   @param {JSON} message: The message that was received from the vehicle
+    */
+  handleReceivedMessage(message) {
+    const srcVehicle = this.selectVehicle(message.srcVehicleID);
+    const ackMessage = { type: 'ack', received: message.type };
+    this.sendMessage(message.srcVehicleID, ackMessage);
+    switch (message.type) {
+      case 'UPDATE' || 'update':
+        if (!srcVehicle.isActive()) {
+          this.sendMessage(message.srcVehicleID, { type: 'STOP' });
+        }
+        break;
+      case 'ACK' || 'ack':
+        srcVehicle.acknowledged(message.type)
+        break;
+      case 'CONNECT' || 'connect':
+        this.addVehicle(new Vehicle(message.srcVehicleID, message.vehicleType, message.jobsAvailable));
+        break;
+      case 'POI' || 'poi':
+        this.nextMissionRequiredData.poi.push(new Point(message.lat, message.lon));
+        break;
+      case 'COMPLETE' || 'complete':
+        srcVehicle.setAvailable(true);
+        this.scheduledMissions[this.currentMission].vehicleUpdate();
+        break;
+      default:
+        Orchestrator.log('Unhandled (bad?) message received from vehicle: ', message.srcVehicleID, '; with contents of :', message);
+        this.sendMessage(message.srcVehicleID, { type: 'badMessage', error: 'Bad message type' });
+    }
   }
 }
