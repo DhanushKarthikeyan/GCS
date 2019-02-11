cb00ed3dde00f0b7e988b5dcbcf522e90641660d
Kedwin Chen <kedwinchen@gmail.com>
2019-02-10 16:46:16 -0800

    20190210 kchen75-orchestrator (MessageHandler) Weekly* update

--- src/main/control/MessageHandler.js
+++ src/main/control/MessageHandler.js
@@ -6,7 +6,7 @@ MESSAGE BASIC FIELDS
 ================================================================================
 {
   vehicleID: {string} (The Vehicle ID from where the message was sent; for the GCS, this will be "GCS")
-  messageID: {int} (currentUnixTime is starting counter (this way old messages cannot be re-used);
+  id: {int} (currentUnixTime is starting counter (this way old messages cannot be re-used);
                     this will be sent to Vehicles using `messageInit` message type)
   type: {string} (Message Type)
 
@@ -26,29 +26,52 @@ export default class MessageHandler {
   }
 
   /**
-   *  Creates an instance of a MessageHandler
-   *  @TODO: complete method
+   * Creates an instance of a MessageHandler
+   * @TODO: complete method
    */
   constructor() {
-    // TODO:
-    // Lists:
-    //    messageQueue
-    //    messagesSent
-    //    messagesReceived
+    /* List of JSON obj., ea. repr. 1 msg. */
+    this.messageOutbox = [];
+    /* List of JSON obj., ea. {ID: , type: } */
+    this.messagesSent = [];
+    /* List of JSON obj., ea. {ID: , type: } */
+    this.messagesReceived = [];
+    this.messageSeed = Math.floor(Date.now() / 1000);
+  }
+
+  rmSentMessage(mID) {
+    /* remove a message with messageID `mID` from the messageOutbox queue */
+    this.messageOutbox = this.messageOutbox.filter( (obj) => {obj.id != mID} );
   }
 
   /**
-   *  Logic for sending message
+   * Logic for sending message
    */
   sendMessageTo( vehicle, message ) {
+    requiredFields = ['id', 'type', 'vehicleID'];
     // TODO:
     // Get message
     // Ensure message format
+    switch (message.type) {
+        case 'ACK':
+            rmSentMessage(message.ackID);
+            break;
+        /**
+         * @TODO: Finish moving stuff over from Orchestrator.js
+         */
+        default:
+            break;
+    }
     // Select vehicle
     // Send message
+    /**
+     * Use the EventsHandler classes and methods to send and wait for this stuff
+     */
+    asyncSendMessage(vehicle, message);
     // Loop until receive ACK
     // Ensure ACK message ID is same as the one sent
     // Add messageID to usedIDs
+    messagesSent.push(message.id);
     // If same, exit
     // If different, raise warning, drop message, keep waiting (?)
     // Retry until timeout; if no reponse, assume dead or jammed
@@ -59,8 +82,12 @@ export default class MessageHandler {
     // Recieve message
     // Check if messageID is already received in `messagesReceived`
     // If so, drop the message and stop processing
+    if( messagesReceived.includes(message.id) || messages. ) {
+        return;
+    }
     // Otherwise:
     // 0. record messageID in messagesReceived
+    messagesReceived.push(message.id);
     // 1. Create appropriate ACK message
     // 2. Send ACK message
     // 3. Handle the message received
@@ -70,4 +97,3 @@ export default class MessageHandler {
     // Determine who should receive the message based on type, contents, etc.
   }
 }
-
